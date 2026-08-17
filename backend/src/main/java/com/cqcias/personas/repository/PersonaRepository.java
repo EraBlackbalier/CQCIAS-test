2829df8af1a9179bdf2b358b34c7ad26a3d176d4
EraBlackbalier <65816092+EraBlackbalier@users.noreply.github.com>
2026-08-16 18:40:31 -0600

    Crea PersonaRepository (#5)

---
+++ backend/src/main/java/com/cqcias/personas/repository/PersonaRepository.java
@@ -0,0 +1,8 @@
+package com.cqcias.personas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cqcias.personas.model.Persona;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
